771d515f105a99b9f248a7f1ef9a52fc907c3c53
Jacob Simonson <jsimonson2013@gmail.com>
2018-02-17 12:15:20 -0700

    First pass at merge sort

--- sortarray.py
+++ sortarray.py
@@ -23,8 +23,27 @@ def selectionSort(array):
 
     return sort
 
+def recursivelyMergeAndSort(array1, array2):
+
+    if (len(array1) < 2 or len(array2) < 2):
+        if int(array1[0]) < int(array2[0]):
+            return array1 + array2
+        else:
+            return array2 + array1
+    else:
+        midptArray1 = int(len(array1)/2)
+        midptArray2 = int(len(array2)/2)
+        return recursivelyMergeAndSort(array1[:midptArray1], array1[midptArray1:]) + recursivelyMergeAndSort(array2[midptArray2:], array2[:midptArray2])
+
+
+
 def mergeSort(array):
-    print("merge sort here")
+    sort = []
+    if (len(array) < 2):
+        return array
+
+    midpt = int(len(array)/2)
+    return recursivelyMergeAndSort(array[:midpt], array[midpt:])
 
 if __name__ == "__main__":
     input = open(sys.argv[1], "r")
@@ -35,4 +54,4 @@ if __name__ == "__main__":
         arrayIn.append(line[:-1])
    
     print(arrayIn)
-    print(selectionSort(arrayIn))
+    print(mergeSort(arrayIn))
